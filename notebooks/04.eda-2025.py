a7d3b85628fde7e1436059636b7a9acc24b648aa
Veru <vdcm1993@protonmail.com>
2026-07-24 03:54:35 +0200

    corregir error pull request

--- notebooks/04.eda-2025.py
+++ notebooks/04.eda-2025.py
@@ -2,16 +2,10 @@
 # %% 
 # import sys
 import sys
-import math
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from utils.load_raw_data import RawData
-from IPython.display import display
-import polars as pl
-import seaborn as sns
-import matplotlib.pyplot as plt
-import altair as alt
 import seaborn as sns
 sns.set_theme(style="darkgrid")
 
